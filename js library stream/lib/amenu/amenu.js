6261c9c303dd10c05ef5e58e67ea93d4bea06190
Roman <77878121+Romdra@users.noreply.github.com>
2021-02-26 16:51:22 +0500

    Add files via upload

---
+++ js library stream/lib/amenu/amenu.js
@@ -0,0 +1,87 @@
+(() => {
+
+   let burgerWidth = 0;
+   let lastWidthItem = 0;
+
+   const init = (menu, menuList, itemsMenu, burgerMenu) => {
+      itemsMenu.forEach(elem => {
+         elem.classList.add('amenu__item');
+      });
+
+      burgerMenu.classList.add('amenu__burger');
+
+      const [burgerBtn, burgerList] = createBurgerMenu(burgerMenu);
+
+      updateMenu(menu, menuList, burgerMenu, burgerBtn, burgerList);
+      
+      window.addEventListener('resize', () => {
+         updateMenu(menu, menuList, burgerMenu, burgerBtn, burgerList);
+      })
+
+   };
+
+   const createBurgerMenu = (parentBurger) => {
+      const burgerBtn = document.createElement('button');
+      parentBurger.append(burgerBtn);
+      burgerBtn.classList.add('amenu__burger-btn');
+
+      burgerBtn.addEventListener('click', () => {
+         parentBurger.classList.toggle('amenu__burger-open')
+      })
+
+      const burgerList = document.createElement('ul');
+      parentBurger.append(burgerList);
+      burgerList.classList.add('amenu__burger-list');
+
+
+
+      return [burgerBtn, burgerList];
+   };
+
+   const updateMenu = (menu, menuList, burgerMenu, burgerBtn, burgerList) => {
+      const menuItems = menuList.querySelectorAll('.amenu__item');
+      const burgerItems = burgerList.querySelectorAll('.amenu__item');
+
+      const widthMenu = menu.offsetWidth;
+
+      burgerWidth = burgerMenu.offsetWidth || burgerWidth;
+
+      const widthAllItems = [...menuItems].reduce((acc, elem) => {
+         return acc + elem.offsetWidth + parseFloat(getComputedStyle(elem).marginRight)
+      }, 0) + burgerWidth
+
+      if (widthMenu < widthAllItems) {
+         const lastItem = menuItems[menuItems.length - 1];
+         lastWidthItem = lastItem.offsetWidth + parseFloat(getComputedStyle(lastItem).marginRight);
+         burgerList.append(lastItem);
+         return updateMenu(menu, menuList, burgerMenu, burgerBtn, burgerList)
+      }
+
+      if (widthMenu > widthAllItems + lastWidthItem && burgerItems.length) {
+         const firstElem = burgerItems[0];
+         menuList.append(firstElem);
+         return updateMenu(menu, menuList, burgerMenu, burgerBtn, burgerList)
+      }
+
+      checkBurgerItems(burgerItems.length, burgerBtn);
+   };
+
+   const checkBurgerItems = (burgerItemsCount, burgerBtn) => {
+      if (burgerItemsCount) {
+         burgerBtn.classList.add('amenu__burger-btn_active')  
+      } else {
+         burgerBtn.classList.remove('amenu__burger-btn_active')
+      }
+   }
+
+   window.amenu = (selectorMenu, selectorMenuList, 
+                selectorIntemsMenu, selectorBurgerMenu) => {
+   const menu = document.querySelector(selectorMenu),
+               menuList = document.querySelector(selectorMenuList),
+               itemsMenu = document.querySelectorAll(selectorIntemsMenu),
+               burgerMenu = document.querySelector(selectorBurgerMenu);
+
+         init(menu, menuList, itemsMenu, burgerMenu);
+   };
+
+})() 
---
+++ js library stream/scripts/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+   amenu('.menu', '.menu__list', '.menu__item', '.menu__burger');
+
+})
+
